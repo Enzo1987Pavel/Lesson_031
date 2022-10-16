3160d48dff6c54bcef725e8d4c720d932b048606
Enzo1987Pavel <Enzo1987@mail.ru>
2022-10-16 17:46:35 +0300

    Create permissions and Final commit

--- avito/urls.py
+++ avito/urls.py
@@ -14,6 +14,7 @@ router.register("location", LocationViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", root),
+
     path("cat/", include("ads.urls.category_urls")),
     path("ad/", include("ads.urls.ad_urls")),
     path("user/", include("users.urls")),
