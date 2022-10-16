6a82cb9ae86429e74af68ea4a81ea670672b5cd3
Enzo1987Pavel <Enzo1987@mail.ru>
2022-10-16 12:25:35 +0300

    Init commit

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -25,18 +26,21 @@ class UserRoles:
     )
 
 
-class User(models.Model):
-    class Meta:
-        verbose_name = "Пользователь"
-        verbose_name_plural = "Пользователи"
+# class User(models.Model):
+#     class Meta:
+#         verbose_name = "Пользователь"
+#         verbose_name_plural = "Пользователи"
+#
+#     first_name = models.CharField(verbose_name="Имя", max_length=100)
+#     last_name = models.CharField(verbose_name="Фамилия", max_length=100)
+#     username = models.CharField(verbose_name="Никнейм", max_length=50, unique=True)
+#     password = models.CharField(verbose_name="Пароль", max_length=50)
+#     role = models.CharField(verbose_name="Группа", choices=UserRoles.choices, default="member", max_length=16)
+#     age = models.PositiveIntegerField(verbose_name="Возраст", null=True)
+#     location = models.ManyToManyField(Location, verbose_name="Местоположение")
+#
+#     def __str__(self):
+#         return self.username
 
-    first_name = models.CharField(verbose_name="Имя", max_length=100)
-    last_name = models.CharField(verbose_name="Фамилия", max_length=100)
-    username = models.CharField(verbose_name="Никнейм", max_length=50, unique=True)
-    password = models.CharField(verbose_name="Пароль", max_length=50)
-    role = models.CharField(verbose_name="Группа", choices=UserRoles.choices, default="member", max_length=16)
-    age = models.PositiveIntegerField(verbose_name="Возраст", null=True)
-    location = models.ManyToManyField(Location, verbose_name="Местоположение")
-
-    def __str__(self):
-        return self.username
+class User(AbstractUser):
+    pass
